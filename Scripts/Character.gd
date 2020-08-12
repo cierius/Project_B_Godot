30e01b0ae94b7f19895ed6370ad2e3148aab3486
Kanin (desktop) <kanin.mann00@gmail.com>
2020-08-12 16:41:27 -0400

    Basic enemy attack is done

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,22 +1,32 @@
 extends KinematicBody2D
 
 export (int) var speed = 200
+export (int) var health = 100
 
 var velocity = Vector2()
+var is_moving = false
 
 onready var cam = get_node("Camera2D")
 
+
 func _process(_delta):
+	if(health <= 0):
+		death()
+	
 	input_detection()
 	velocity = move_and_slide(velocity)
-	#print (get_node("Camera2D").get_camera_position())
 	
+	if(velocity.x != 0 or velocity.y != 0):
+		#print("moving")
+		is_moving = true
+	else:
+		is_moving = false
 
 func input_detection():
 	# Need to zero out the vector otherwise it keeps going the same direction
 	velocity = Vector2()
 	
-	# Basic input
+	# Keyboard input
 	if(Input.is_action_pressed("move_up")):
 		velocity.y += -1
 	if(Input.is_action_pressed("move_down")):
@@ -29,3 +39,25 @@ func input_detection():
 	# Normalized makes it so that velocity clamps to 1
 	velocity = velocity.normalized() * speed 
 	
+	if(Input.is_action_pressed("lmb")):
+		mouse_click(0)
+	elif(Input.is_action_pressed("rmb")):
+		mouse_click(1)
+	
+func mouse_click(button):
+	if(button == 0):
+		attack(get_viewport().get_mouse_position())
+	elif(button == 1):
+		print("RMB")
+
+func attack(mouse_pos):
+	look_at(mouse_pos)
+
+# Just messing around with a death message and figuring out how to create
+# instances using godot
+func death():
+	print("Death")
+	var death_text = Label.new()
+	death_text.set_position(Vector2(get_viewport().size.x/2, get_viewport().size.y/2))
+	death_text.set_text("You have died")
+	get_parent().add_child(death_text)
--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,31 +1,61 @@
 extends KinematicBody2D
 
+export (int) var health = 100
+export (int) var speed = 125
+export (float) var absorb_rate = 1.05
+export (int) var attack_rate = 2
 
-export (int) var speed = 175
 
+var dist = 0
 var target = Vector2()
 var dir = Vector2()
+
+var has_attacked = false
+var attack_timer = 0
 
 func _ready():
 	target = get_parent().get_node("Character")
 
 
-func tracking_ai():
-	dir = target.position - position
-	
-	print(dir)
-	
-	if(abs(dir.x) <= 32 or abs(dir.y) <= 32):
-		dir = dir.normalized() * speed * 0.5
-		
-		get_node("CollisionShape2D").z_index = 1
-	else:
-		dir = dir.normalized() * speed
-		
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	tracking_ai()
+	dir = move_and_slide(dir)
+
+
+func tracking_ai():
+	dir = target.position - position
 	
-	move_and_slide(dir)
+	dist = global_position.distance_to(target.position)
+	
+	# Close to the player the enemy will match the speed and "absorb" the player
+	if(dist <= 64):
+		if(dist <= 10):
+			if(has_attacked == false):
+				target.health -= 25
+				print("Attacked player hp remaining: ", target.health)
+				has_attacked = true
+				attack_timer = OS.get_unix_time()
+			else:
+				attack_timer()
+			
+			
+		if(target.is_moving == true):
+			dir = dir.normalized() * target.speed * absorb_rate
+		else:
+			# 25 seems like a good amount so that it absorbs quicker when the
+			# player is standing still
+			dir = dir.normalized() * absorb_rate * 25
+		
+		# Enemies will not collide with each other
+		set_collision_mask_bit(1, false)
+	else:
+		dir = dir.normalized() * speed
+		set_collision_mask_bit(1, true)
+		
+
+func attack_timer():
+	var elapsed = OS.get_unix_time() - attack_timer
+	if(elapsed >= attack_rate):
+		has_attacked = false
+		print("attack reset")
