b959cbf0a4d7d813f8ac4a15ede36a9c7412538d
Kanin (Laptop) <kanin.mann00@gmail.com>
2020-08-12 13:28:25 -0400

    Basic enemy tracking ai

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,16 +1,31 @@
-extends CollisionShape2D
+extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (int) var speed = 175
 
+var target = Vector2()
+var dir = Vector2()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	target = get_parent().get_node("Character")
+
+
+func tracking_ai():
+	dir = target.position - position
+	
+	print(dir)
+	
+	if(abs(dir.x) <= 32 or abs(dir.y) <= 32):
+		dir = dir.normalized() * speed * 0.5
+		
+		get_node("CollisionShape2D").z_index = 1
+	else:
+		dir = dir.normalized() * speed
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	tracking_ai()
+	
+	move_and_slide(dir)
